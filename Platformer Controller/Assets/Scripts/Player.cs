e5cc2df2ee7b048d0b15202b83d68a87cbac1858
UnderEngineeredCode <80602321+UnderEngineeredCode@users.noreply.github.com>
2021-03-14 13:41:07 -0400

    Added Simple Movement
    Movement works, but still needs:
    -Acceleration/Deceleration
    -Stronger Gravity from SO
    -No infinite Jump

--- Platformer Controller/Assets/Scripts/Player.cs
+++ Platformer Controller/Assets/Scripts/Player.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
 
 	[SerializeField] CharacterPhysics playerPhysics;
+
+	Vector2 moveDir;
 
 	Rigidbody2D rb;
 
@@ -14,5 +17,31 @@ public class Player : MonoBehaviour
 		rb = GetComponent<Rigidbody2D>();
 	}
 
+	private void FixedUpdate()
+	{
+		float targetvelocity = moveDir.x * playerPhysics.MaxSpeed;
+		rb.velocity = new Vector2(targetvelocity, rb.velocity.y);
+	}
+
+	public void OnMove(InputAction.CallbackContext context)
+	{
+		Debug.Log("is moving");
+		moveDir = context.ReadValue<Vector2>();
+	}
+
+	public void OnJump(InputAction.CallbackContext context)
+	{
+		if (context.started)
+		{
+			Debug.Log("is jumping");
+			rb.gravityScale = 1f;
+			rb.velocity = new Vector2(rb.velocity.x, playerPhysics.MinJumpVelocity);
+		}
+		else if (context.canceled)
+		{
+			Debug.Log("stopped jumping");
+			rb.gravityScale = playerPhysics.FastFallFactor;
+		}
+	}
 
 }
--- Platformer Controller/Assets/Scripts/Scriptable Objects/CharacterPhysics.cs
+++ Platformer Controller/Assets/Scripts/Scriptable Objects/CharacterPhysics.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = ("Character Physics"))]
 public class CharacterPhysics : ScriptableObject
 {
 	[Header("Player Ground Physics")]
