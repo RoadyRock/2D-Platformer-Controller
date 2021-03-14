5b8581a2d4bbadf78147b8f52032ab234d43ab15
UnderEngineeredCode <80602321+UnderEngineeredCode@users.noreply.github.com>
2021-03-14 17:14:46 -0400

    Created a Gravity Setter script

---
+++ Platformer Controller/Assets/Scripts/GravitySetter.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravitySetter : MonoBehaviour
+{
+	[SerializeField] CharacterPhysics physics;
+
+	private void Start()
+	{
+		ResetGravity();
+	}
+
+	public void ResetGravity()
+	{
+		SetGravity(Vector2.down, physics.Gravity);
+	}
+
+	public void SetGravity(Vector2 direction, float magnitude)
+	{
+		Physics2D.gravity = direction.normalized * magnitude;
+	}
+}
